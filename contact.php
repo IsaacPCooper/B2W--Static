32f09ad95be96c3b4bee941f5a4fad655f3cfd7b
Isaac Philip Cooper <Jordan.P.C.Austin@gmail.com>
2020-07-01 12:29:48 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -65,10 +65,10 @@
               <!--navbar-header-->
               <div class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
-                  <li><a href="index.html">Home</a></li>
-                  <li><a href="blog.html">Blog</a></li>
-                  <li><a href="resources.html">Resources</a></li>
-                  <li class="active"><a href="contact.html">Contact</a></li>
+                  <li><a href="index.php">Home</a></li>
+                  <li><a href="blog.php">Blog</a></li>
+                  <li><a href="resources.php">Resources</a></li>
+                  <li class="active"><a href="contact.php">Contact</a></li>
                 </ul>
               </div>
               <!--navbar-collapse-->
